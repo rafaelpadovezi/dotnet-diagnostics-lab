0e5d0db29fc46585639c44773f4805ad746e4801
Rafael Miranda <rafaelpadovezi@gmail.com>
2024-01-07 22:40:17 +0000

    feat: multiple enumeration sample

--- src/Controllers/v1/DiagnosticsController.cs
+++ src/Controllers/v1/DiagnosticsController.cs
@@ -30,4 +30,43 @@ public class DiagnosticsController : ControllerBase
         await _context.Database.ExecuteSqlRawAsync("WAITFOR DELAY '00:00:00.500'");
         return Ok();
     }
+
+    [HttpGet("enumeration")]
+    public IActionResult GetMultipleEnumerations([FromQuery] bool multiple = false)
+    {
+        var enumerable = Enumerable.Range(0, 10000).Select(x => new SomeClass
+        {
+            Id = x,
+            Name = $"Name {x}"
+        });
+        enumerable = enumerable.Where(x => x.Id % 10 == 0);
+
+        if (multiple)
+        {
+            return Ok(GetValues(enumerable));
+        }
+
+        var list = enumerable.ToList();
+
+        return Ok(GetValues(list));
+    }
+
+    private List<int> GetValues(IEnumerable<SomeClass> enumerable)
+    {
+        var returnValues = new List<int>();
+        foreach (var i in Enumerable.Range(1, 1000))
+        {
+            var value = enumerable.SingleOrDefault(y => y.Id == i);
+            if (value is not null)
+                returnValues.Add(value.Id);
+        }
+
+        return returnValues;
+    }
+}
+
+class SomeClass
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
 }
